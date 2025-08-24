b8e08cb5267909fbc336c7a860dfa3584ab9a300
Amar <jeetamar496@gmail.com>
2025-08-25 01:07:07 +0530

    italic font style added to the about me page content

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -6,32 +6,32 @@ const page = () => {
       <div className=" mt-12  w-full flex flex-col items-center below-700:mt-[4rem] below-635:mt-[7rem]  ">
         <h1 className="text-[6rem] uppercase below-1000:text-[4rem] below-550:text-[2rem] below-500:text-[1.1rem] text-green-400">About me</h1>
         <div className=" sectionBorder w-[80vw]  ml-[12rem] below-500:ml-0 below-500:px-3 below-550:mt-[2rem]    below-1100:ml-0  px-8   ">
-          <p className=" my-4 mt-[3rem] text-sm below-500:mx-0 uppercase below-500:text-xs text-green-100 ">
+          <p className=" my-4 mt-[3rem] text-sm below-500:mx-0 uppercase below-500:text-xs text-green-100 italic ">
             My Name is Amarjeet Singh , Currently a fullStack web developer with
             a keen interest in making beautiful UI based websites and app .
           </p>
 
-          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-0 text-green-100 ">
+          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-0 text-green-100 italic ">
             So It Took me 5 years to realize my love and passion for Fullstack
             developement as a job since i already have 3.5 years of experience
             in customer success and travel consultant roles but i didn't get
             that freedom to be creative and to create the things what i imagine
             and bring that into reality .
           </p>
-          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-5 text-green-100">
+          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-5 text-green-100 italic">
             Since the experience from my previous jobs have helped me in
             creating products which the customer like and seeing the product
             from their perspective hence this job makes me more creative in
             delivering success.
           </p>
 
-          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-5 text-green-100">
+          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-5 text-green-100 italic">
             I also deal with ADHD which initially was a barrier for me as i
             couldn't concentrate properly not even for 10 minutes straight … but
             as soon as i started to learn web development it became a ritual to
             practice concentration as a job 
           </p>
-          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-5 text-green-100">
+          <p className=" my-4 mt-[3rem] text-sm uppercase below-500:text-xs below-500:my-5 text-green-100 italic">
           slowly and gradually after 8 months
             i got good at it and now i really feel that those 8 months when the
             progress was nearly negligible while learning web devlopment those 8
@@ -44,7 +44,7 @@ const page = () => {
           How long did it took for me to get confident in  FullStack web Devlopment ? 
 
           </p>
-          <p className=" my-4 text-sm uppercase below-500:text-xs below-500:my-5 text-green-100">
+          <p className=" my-4 text-sm uppercase below-500:text-xs below-500:my-5 text-green-100 italic">
           1.3 years , I started to learn fullstack in mid of  2023 
 
           </p>
@@ -54,7 +54,7 @@ const page = () => {
         What do I build?
 
           </p>
-          <p className=" my-4 text-sm uppercase below-500:text-xs text-green-100">
+          <p className=" my-4 text-sm uppercase below-500:text-xs text-green-100 italic">
           anything that is related to tech and softwares  as of now i have built fullstack web apps and mobile clones using react native  much more to build in future :)
 
           </p>
@@ -63,7 +63,7 @@ const page = () => {
         <p className=" text-lg uppercase below-500:text-sm text-green-300 ">
         What do you do when frustrated ?
           </p>
-          <p className=" my-4 text-sm uppercase below-500:text-xs text-green-100">
+          <p className=" my-4 text-sm uppercase below-500:text-xs text-green-100 italic">
           I go  for a long run , do extensive workout also play a lot of racing games and shooter like DOOM on repeat .These things really help me with my frustration  :) 
           </p>
         </div>
@@ -71,7 +71,7 @@ const page = () => {
         <p className=" text-lg uppercase below-500:text-sm text-green-300">
         What are your hobbies?
           </p>
-          <p className=" my-4 text-sm uppercase below-500:text-xs text-green-100">
+          <p className=" my-4 text-sm uppercase below-500:text-xs text-green-100 italic">
           I love outdoor sports like running , swimming , badminton, solo bike rides, hiking  calistenics  and playing PC games too I am a born Gamer  i love playing RPG games like GTA ,Witcher ,Far Cry etc and much more .
           </p>
         </div>
