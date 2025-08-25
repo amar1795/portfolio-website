b2a64ff27fc196b73ab40314fc525e41c1fd2d6c
Amar <jeetamar496@gmail.com>
2025-08-25 17:19:41 +0530

    sidelinks and contact me page made mobile responsive

--- src/components/sideLinks.tsx
+++ src/components/sideLinks.tsx
@@ -62,26 +62,26 @@ const SideLinks = () => {
             <button className="close-button" onClick={() => setIsOpen(false)}>
               X
             </button>
-            <div className="flex flex-col justify-between  items-center mt-[3rem] w-full    ">
+            <div className="flex flex-col  justify-between  items-center mt-[10rem] w-full    ">
               <Link href="/">
-                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 my-2 w-[10rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>Home</button>
+                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 my-2 w-[10rem] text-[0.4rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>Home</button>
               </Link>
               <Link href="/tech-stack">
-                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>
+                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2 text-[0.4rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>
                   Tech Stack
                 </button>
               </Link>
               <Link href="/projects">
-                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>Projects</button>
+                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2 text-[0.4rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>Projects</button>
               </Link>
               <Link href="/resume">
-                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>Resume</button>
+                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2 text-[0.4rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>Resume</button>
               </Link>
               <Link href="/about-me">
-                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>About Me</button>
+                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2 text-[0.4rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>About Me</button>
               </Link>
               <Link href="/contact-me">
-                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>
+                <button className="border-2 border-green-400/80 text-green-300 px-5 py-2 text-xs uppercase rounded transition-all duration-300 hover:bg-green-400 hover:text-black hover:border-green-400 hover:shadow-lg hover:shadow-green-400/50 shadow-md shadow-green-400/30 w-[10rem] my-2 text-[0.4rem]" style={{ boxShadow: '0 0 10px rgba(34, 197, 94, 0.5)' }} onClick={() => setIsOpen(false)}>
                   Contact Me
                 </button>
               </Link>
